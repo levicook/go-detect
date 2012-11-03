2d731e55787e73f047b509c25e6027340bfcb8ec
Levi Cook <levicook@gmail.com>
2012-11-03 12:32:33 -0600

    first non-zero time

--- first.go
+++ first.go
@@ -1,5 +1,7 @@
 package detect
 
+import "time"
+
 const zeroString = ""
 
 func String(strings ...string) string {
@@ -9,4 +11,13 @@ func String(strings ...string) string {
 		}
 	}
 	return zeroString
+}
+
+func Time(times ...time.Time) time.Time {
+	for _, t := range times {
+		if !t.IsZero() {
+			return t
+		}
+	}
+	return time.Time{}
 }
